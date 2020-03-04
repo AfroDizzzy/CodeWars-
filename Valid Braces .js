a6e6650690f89060058c5540b6c94039c898563d
AfroDizzzy <60550708+AfroDizzzy@users.noreply.github.com>
2020-03-04 22:40:49 +0100

    Update Valid Braces .js

--- Valid Braces .js
+++ Valid Braces .js
@@ -1,21 +1,45 @@
+// Description NOTE - THE tests provided for this kata is wrong. They contridict the examples given below. My solution works according to the examples and description. 
+
+/*
+Write a function that takes a string of braces, and determines if the order of the braces is valid. It should return true if the string is valid, and false if it's invalid.
+
+This Kata is similar to the Valid Parentheses Kata, but introduces new characters: brackets [], and curly braces {}. Thanks to @arnedag for the idea!
+
+All input strings will be nonempty, and will only consist of parentheses, brackets and curly braces: ()[]{}.
+
+What is considered Valid?
+A string of braces is considered valid if all braces are matched with the correct brace.
+
+Examples
+"(){}[]"   =>  True
+"([{}])"   =>  True
+"(}"       =>  False
+"[(])"     =>  False
+"[({})](]" =>  False
+
+*/
+
 function validBraces(braces){
   let newS = braces.split("");
-  let match1 = "[]";
-  let match2 = "{}";
-  let match3 = "()";
-  for (i = 0; i < braces.length; i ++) {
-    console.log(i);
-    if (newS.length % 2 > 0) {
-         return false;
+  let bracer = { "[" : "]", "(" : ")", "{": "}" }
+  
+   if (newS.length == 2) { 
+      if (bracer[newS[0]] == newS[1]) {
+      return true; 
+      } else {
+        return false;
+      }
     }
     
-    if (newS[i] + newS[newS.length - 1 - i] == match1 || match2 || match3 ){
-      console.log(newS[i] + " and " + newS[newS.length - 1 - i]);
-      if (i == newS.length / 2) {
-        return true; 
-      }
-    } else {
-      	return false;
+    if (newS.length % 2 > 0) {
+        return false;
     }
+    
+  for (i = 0; i < newS.length; i ++) {   
+       if (bracer[newS[i]] == newS[i++]) {
+       let newStringToTest =  newS.slice(0, i) + newS.slice(i + 2);
+       return validBraces(newStringToTest);
+      }
   }
+  return false; 
 }
